4464c4ea66f57ebc47591b58626cd59c323a28d5
DCANascimento <152305663+DCANascimento@users.noreply.github.com>
2025-11-28 12:13:10 +0000

    minor

---
+++ Placeholder/test1/src/main/java/test1/test1/APIController.java
@@ -0,0 +1,34 @@
+package test1.test1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+	//All this an example
+	//all this through services ample
+	//bars
+
+	//@GetMapping("/something")
+	//public List<Something> allSomethings() {
+	//	return everything
+	//}
+
+	//@PostMapping("/something")
+	//public ResponseEntity<?> addBooking(@RequestBody Something smth) {
+	//	add something to the database
+	//}
+
+	//@DeleteMapping("/something/{token}/delete")
+	//public Something deleteSomething(@PathVariable String token) {
+		// delete something from the database logic
+	//}
+
+	//@PutMapping("/bookings/{token}/update")
+	//public BookingRequest updateBookingRequest(@PathVariable String token) {
+		// update something in the database
+	//}
+
+}
